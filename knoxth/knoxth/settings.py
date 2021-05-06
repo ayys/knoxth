c1c4c52b211a295c18bc426d692ba01c63e7bef6
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-06 18:42:43 +0545

    added configuration related to drf in settings

--- knoxth/knoxth/settings.py
+++ knoxth/knoxth/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
