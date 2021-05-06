4b03f3fd4b8496ba5863caf1cd15d8eaa7f4acdb
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-06 19:32:27 +0545

    added testcases for scope model
    - create scope with no context
    - create scope with no permissions
    - add unknown permission to scope
    - add known permission to scope
    - delete unknown permission from scope
    - delete known permission from scope

--- knoxth/scopes/tests.py
+++ knoxth/scopes/tests.py
@@ -26,3 +26,36 @@ class ContextTestCase(TestCase):
     def test_context_name_is_unique(self):
         with self.assertRaises(IntegrityError):
             Context.objects.create(name="home")
+
+
+class ScopeTestCase(TestCase):
+    def setUp(self):
+        self.context = Context.objects.create(name="Hello")
+        self.scope = Scope.objects.create(context=self.context,
+                                          permissions=constants.ACCESS)
+
+    def test_scope_with_no_context(self):
+        with self.assertRaises(IntegrityError):
+            Scope.objects.create(permissions=constants.ACCESS)
+
+    def test_scope_with_no_perrmissions(self):
+        with self.assertRaises(IntegrityError):
+            Scope.objects.create()
+
+    def test_add_unknown_perm_to_scope(self):
+        self.scope.add_perm(78)       # throw  error
+        self.assertEqual(self.scope.permissions,
+                         constants.ACCESS)
+
+    def test_add_known_perm_to_scope(self):
+        self.scope.add_perm(constants.MODIFY)       # throw  error
+        self.assertEqual(self.scope.permissions,
+                         constants.ACCESS | constants.MODIFY)
+
+    def test_del_known_perm_from_scope(self):
+        self.scope.del_perm(constants.ACCESS)
+        self.assertEqual(self.scope.permissions, 0)
+
+    def test_del_unset_perm_from_scope(self):
+        self.scope.del_perm(constants.MODIFY)
+        self.assertEqual(self.scope.permissions, constants.ACCESS)
