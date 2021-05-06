48f37f531590f8232f6af6b5779233c0d64857c6
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-06 19:34:17 +0545

    make the default none so that it throws error when no name specified

--- knoxth/scopes/models.py
+++ knoxth/scopes/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 # Create your models here.
 class Context(models.Model):
-    name = models.CharField(max_length=64, unique=True)
+    name = models.CharField(
+        default=None,
+        max_length=64,
+        unique=True)
 
     def __str__(self):
         return self.name
