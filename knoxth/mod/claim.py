5118e6184a354d34408ce2f3eaac29fc30ce583a
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-06 21:34:13 +0545

    fixed indentation in claims model

--- knoxth/mod/claim.py
+++ knoxth/mod/claim.py
@@ -15,18 +15,23 @@ class Claim(models.Model):
     scopes = models.ManyToManyField(Scope)
 
     def add_scope(self, context, permissions):
-        return self.scopes.get_or_create(context=Context.objects.get_or_create(name=context)[0],
-                                         permissions=permissions)[0]
+        return self.scopes.get_or_create(
+            context=Context.objects.get_or_create(name=context)[0],
+            permissions=permissions)[0]
 
     def del_scope(self, context, permissions=ACCESS|MODIFY|DELETE):
-        return self.scopes.filter(context__name=context, permissions=permissions).delete()
+        return self.scopes.filter(
+            context__name=context,
+            permissions=permissions).delete()
 
     def del_scope_with_context(self, context):
         return self.scopes.filter(context__name=context).delete()
 
-    def verify(self, permission, context):
-        return len([_.permissions & permission for _ in Scope.objects.filter(context__name=context)
-                    if _.permissions & permission != 0]) > 0
+    def verify(self, context, permission):
+        return len(
+            [_.permissions & permission
+             for _ in Scope.objects.filter(context__name=context)
+             if _.permissions & permission != 0]) > 0
 
     def __str__(self):
         return f"Token {self.token.digest[:5]}... has {[_ for _ in self.scopes.all()]} scopes"
