feeb03f6935c7f770197c31aaae1cff50e5e521a
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-06 19:01:04 +0545

    context must have unique name

--- knoxth/scopes/models.py
+++ knoxth/scopes/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Context(models.Model):
-    name = models.CharField(
-        max_length=64)
+    name = models.CharField(max_length=64, unique=True)
 
     def __str__(self):
         return self.name
