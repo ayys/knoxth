9e18fbc66d0716c9cc32d8c106f2ec847d850d6e
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-07 15:23:40 +0545

    added view for listing contexts

--- knoxth/views.py
+++ knoxth/views.py
@@ -1,3 +1,26 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from knox.views import LoginView as KnoxLoginView
+from rest_framework import viewsets
+from rest_framework.authentication import \
+    TokenAuthentication as DRFTokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+
+from knoxth.models import Context
+from knoxth.serializers import ContextSerializer
+
+
+class ContextViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    A simple ViewSet for viewing accounts.
+    """
+    queryset = Context.objects.all()
+    serializer_class = ContextSerializer
+    permission_classes = [IsAuthenticated]
+
+
 class KnoxthLoginView(KnoxLoginView):
     '''
     The Login view has to be authenticated with the authorization code
