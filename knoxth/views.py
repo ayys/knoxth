5060bcfae319f93a2c25179bea107f9b32e67cfb
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-07 15:23:15 +0545

    added knoxth login view
    this view takes authorization code and spits out access token

--- knoxth/views.py
+++ knoxth/views.py
@@ -1,3 +1,9 @@
+class KnoxthLoginView(KnoxLoginView):
+    '''
+    The Login view has to be authenticated with the authorization code
+    acquired via DRF.
+    '''
+    authentication_classes = [DRFTokenAuthentication]
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
