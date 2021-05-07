ffc67527270d0c7e566194f0ac1b1403befdfadc
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-07 18:55:16 +0545

    make decorator with without parameterrs

--- knoxth/decorators.py
+++ knoxth/decorators.py
@@ -5,23 +5,23 @@ from knoxth.auth import IsScoped
 from knoxth.models import Context
 
 
-def withContext(context_name=None):
+def withContext(_class=None, context=None):
     def _withContext(cls):
+        assert callable(_class) or _class is None
         @wraps(cls, updated=())
         class _Decorated(cls):
             def __init__(self, *args, **kwargs):
                 super(cls, self).__init__(*args, **kwargs)
                 SWSIsScoped = IsScoped
 
-                if context_name:
-                    _context_name = context_name
+                if context:
+                    _context_name = context
                 else:
                     _context_name = cls.__name__\
                                        .replace("ViewSet", "")\
                                        .lower()
-
                 SWSIsScoped.context = _context_name
                 self.permission_classes.append(SWSIsScoped)
                 Context.objects.get_or_create(name=_context_name)
         return _Decorated
-    return _withContext
+    return _withContext(_class) if callable(_class)else _withContext
