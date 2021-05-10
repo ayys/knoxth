ecd0969462072d66179cfaa45dd2eb514ef85871
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-10 19:50:22 +0545

    using pylintrc for linting with flycheck in emacs

---
+++ knoxth/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode . ((flycheck-flake8rc . ".flake8")
+                 (flycheck--automatically-enabled-checkers . (python-flake8 python-pylint)))))
