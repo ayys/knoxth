767f45a6fec3f50f65d190eeab67073d3bf641de
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-15 15:49:48 +0545

    Wrapped it in str because older versions of Python don't work properly

--- knoxth_project/settings.py
+++ knoxth_project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "knoxth_project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
 
