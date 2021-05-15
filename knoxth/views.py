d0b6067f5bb16e36113424af99188b131e3dab42
आयुष झा <ayushjha+gitlab@pm.me>
2021-05-15 16:29:18 +0545

    added get_queryset and perform_save actions for the viewset

--- knoxth/views.py
+++ knoxth/views.py
@@ -4,10 +4,14 @@ Views for knoxth
 from knoxth.views import ContextViewSet
 from knxoth.views import KnoxthLoginView
 """
+import itertools
 
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
+from knox.models import AuthToken
 from knox.views import LoginView as KnoxLoginView
 from rest_framework import mixins, viewsets
 from rest_framework.authentication import (
@@ -17,8 +21,8 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from knoxth.models import Context
-from knoxth.serializers import ContextSerializer
+from knoxth.models import Claim, Context, Scope
+from knoxth.serializers import ContextSerializer, ScopeSerializer
 
 
 class ContextViewSet(viewsets.ReadOnlyModelViewSet):
@@ -47,8 +51,20 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
     """
     Token.objects.filter(user=instance).delete()
     Token.objects.create(user=instance)
+
+
 class AuthTokenViewset(
     mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
 ):
     permission_classes = [IsAuthenticated]
     serializer_class = ScopeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        auth_tokens = AuthToken.objects.filter(user=self.request.user)
+        claims = Claim.objects.filter(token__in=auth_tokens)
+        scope_pks = [[scope.pk for scope in claim.scopes.all()] for claim in claims]
+        normalized_scope_pks = list(itertools.chain.from_iterable(scope_pks))
+        return Scope.objects.filter(pk__in=normalized_scope_pks)
